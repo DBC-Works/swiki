4b4328173e89502c9becba138be871b46771b5ec
DBC-Works <contact.d.b.c@gmail.com>
2024-11-04 15:41:50 +0900

    feat: add page content viewer component

---
+++ src/views/molecules/PageContentViewer.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+
+import { describe, expect, it } from 'vitest'
+import { setupComponentUnderTest } from '../testUtils'
+
+import { PageContentViewer } from './PageContentViewer'
+
+describe('PageContentViewer component', () => {
+  const setup = (content: string) =>
+    setupComponentUnderTest(<PageContentViewer>{content}</PageContentViewer>)
+
+  it('should contain the article element with a lang attribute', () => {
+    // arrange & act
+    setup('')
+
+    // assert
+    expect(screen.getByRole('article')).toHaveAttribute('lang')
+  })
+
+  it('should translate markdown text to HTML', () => {
+    // arrange & act
+    setup(`
+## Page title
+      `)
+
+    // assert
+    expect(screen.getByRole('heading', { level: 2, name: 'Page title' })).toBeInTheDocument()
+  })
+})
--- src/views/pages/FrontPage.tsx
+++ src/views/pages/FrontPage.tsx
@@ -1,24 +1,19 @@
+import { useTranslation } from 'react-i18next'
+
+import { PageContentViewer } from '../molecules/PageContentViewer'
 import { Page } from '../templates/Page'
 
 /**
  * FrontPage component
  * @returns JSX Element
  */
-export const FrontPage: React.FC = (): JSX.Element => (
-  <Page>
-    <article>
-      <h2>FrontPage</h2>
-      <p>
-        &ldquo;swiki&rdquo;(&ldquo;s&rdquo; means &ldquo;solo&rdquo; or &ldquo;single&rdquo;) is a
-        personal wiki application that runs in your browser without an internet connection and
-        stores data locally.
-      </p>
+export const FrontPage: React.FC = (): JSX.Element => {
+  const { t } = useTranslation()
+  const frontPageContent = t('initialFrontPage')
 
-      <h3>CAUTION</h3>
-      <p>
-        swiki uses localStorage to store data. LocalStorage is a not persistent storage, the browser
-        will delete data if you do not use for a while.
-      </p>
-    </article>
-  </Page>
-)
+  return (
+    <Page>
+      <PageContentViewer>{frontPageContent}</PageContentViewer>
+    </Page>
+  )
+}
